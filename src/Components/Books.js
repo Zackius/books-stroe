89016f496479cb2dd450d69a9e8bf5bd94ac1f30
Zacharia <ndunguzachary24@gmail.com>
2022-09-13 08:44:30 +0300

    adding an update feature on the delete function

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -5,13 +5,12 @@ import "./Books.css";
 const Books = () => {
   const [books, setBooks] = useState([]);
 
-  function handleDelete( id ) {
+  function handleDelete(id) {
     if (window.confirm("Are you sure you want to delete this book?")) {
-        axios.delete(`http://localhost:9292/books/${id}`)
-            .then((response) => {
-                console.log(response.data);        
-            });
-        
+      axios.delete(`http://localhost:9292/books/${id}`).then((response) => {
+        console.log(response.data);
+        getAllBooks();
+      });
     }
   }
 
@@ -38,8 +37,14 @@ const Books = () => {
             <img src={book.book_image} alt="book" />
             <h4> Book Name :{book.books_name}</h4>
             <h5> Author :{book.author} </h5>
-                <button onClick={()=>{handleDelete(book.id)}}>Delete</button>
-                <button>Edit</button>
+            <button
+              onClick={() => {
+                handleDelete(book.id);
+              }}
+            >
+              Delete
+            </button>
+            <button>Edit</button>
           </div>
         );
       })}
